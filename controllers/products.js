62d81f06b37401dda9ed275030a36243a5083449
Amir Haytham Salama <63167915+AmirHaytham@users.noreply.github.com>
2020-08-01 06:18:09 +0200

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res, next) => {
+exports.getAddedProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
