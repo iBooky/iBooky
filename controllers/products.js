98e341c8e11b47acd00f76aead6517f49492cddf
Amir Haytham Salama <63167915+AmirHaytham@users.noreply.github.com>
2020-08-01 06:19:31 +0200

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 
-exports.getAddedProduct = (req, res, next) => {
+/*
+exports.getAddProduct = (req, res, next) => {
   res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
@@ -10,7 +11,39 @@ exports.getAddedProduct = (req, res, next) => {
   });
 };
 
-exports.postAddedProduct = (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
+
+
+*/
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
